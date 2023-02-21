598494dc9b08f43df3816ca0e8d576ac90637fd8
Aman Sharma <halfengiiineeer2022@gmail.com>
2023-02-21 23:26:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Bar1 from './Bar1'
+import './index.css'
+
+
 
 function App() {
+  const [val, setVal] = useState('')
+  // const data = ["Stock1","Stock2","Stock3","Stock4"]
+
+const [show,setShow] = useState(false)
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='container'>
+        <img src="https://i.pinimg.com/originals/e9/e2/78/e9e2787d0cb55d570fe1c76843506759.jpg" height={100} width={100} className='imga' alt="No image" />
+        <div className='main'>
+          <input list='data' onChange={(e) => setVal(e.target.value)} placeholder='Search Here' />
+          <datalist id='data'>
+            <button onClick={()=>{setShow(!show)}}><option>Stock1</option></button>
+            <button onClick={()=>{setShow(!show)}}><option>Stock2</option></button>
+            <button onClick={()=>{setShow(!show)}}><option>Stock3</option></button>
+            <button onClick={()=>{setShow(!show)}}><option>Stock4</option></button>
+            <button onClick={()=>{setShow(!show)}}><option>Stock5</option></button>
+          </datalist>
+          <h1 id='val'>{val}</h1>
+        </div>
+          <button onClick={()=>{setShow(!show)}}>Submit</button>
+      {show && <Bar1/>}
+      </div>
+          
+
+    
+
+    </>
+  )
 }
+
 
 export default App;
